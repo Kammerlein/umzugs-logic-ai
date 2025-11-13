03c0f593bbbcf5a0948972ffdabb66fc600b7dfa
Cas_andr <waldmarwo@gmail.com>
2025-11-13 20:51:38 +0200

    Import glass-effects.css for styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import AuthPage from "./pages/AuthPage";
-
+import "./styles/glass-effects.css";
 const queryClient = new QueryClient();
 
 const App = () => (
